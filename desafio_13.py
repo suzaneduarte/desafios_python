6d54d8d160458e5b8001026c6fd13dfc34327b00
Suzane Duarte <ssuzane9@hotmail.com>
2021-11-05 12:13:15 -0300

    desafio 11

---
+++ desafio_13.py
@@ -0,0 +1,13 @@
+idade= int(input())
+
+idade2=(idade/365)
+resto=(idade%365)
+
+idade3=(resto/30)
+resto=(idade3%30)
+
+idade4=(resto)
+
+print=(idade2,'ano(s)')
+print=(idade3,'mes(es)')
+print=(idade4,'dia(s)')
