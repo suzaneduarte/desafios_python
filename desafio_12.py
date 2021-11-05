1827b77c0e13cadf2b8db37136bdbd516a5a47b2
Suzane Duarte <ssuzane9@hotmail.com>
2021-11-05 00:57:19 -0300

    desafio 12

---
+++ desafio_12.py
@@ -0,0 +1,11 @@
+valor = int(input())
+
+cedulas = [100, 50, 20, 10, 5, 2 , 1]
+
+print(valor)
+
+for cedula in cedulas:
+  qt_cedulas = int(valor / cedula)
+  print("{} nota(s) de R$ {},00".format(qt_cedulas, cedula))
+  valor -= qt_cedulas * cedula
+  
