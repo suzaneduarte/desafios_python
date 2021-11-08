cfd2f6d8fa31d35382fe08a539c6a5fcf9281c06
Suzane Duarte <ssuzane9@hotmail.com>
2021-11-08 17:07:30 -0300

    1042 - Sort Simples

---
+++ desafio_17.py
@@ -0,0 +1,15 @@
+valor0, valor1, valor2= input().split(' ')
+valor0= int(valor0)
+valor1= int(valor1)
+valor2= int(valor2)
+
+sequencia= [valor0,valor1,valor2]
+sequencia.sort()
+
+print(sequencia[0])
+print(sequencia[1])
+print(sequencia[2])
+print('')
+print(valor0)
+print(valor1)
+print(valor2)
