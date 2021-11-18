8e0909eadbac388a10662bf4cc37080c645dbd45
Suzane Duarte <ssuzane9@hotmail.com>
2021-11-17 23:28:13 -0300

    2344 - Notas da Prova

---
+++ desafio_38.py
@@ -0,0 +1,16 @@
+nota= int(input())
+
+if nota >= 86 and nota <=100:
+    print('A')
+
+elif nota >= 61 and nota <=85: 
+    print('B')
+    
+elif nota >= 36 and nota <=60: 
+    print('C')    
+    
+elif nota >= 1 and nota <=35: 
+    print('D')   
+    
+else: 
+    print('E')
